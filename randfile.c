f4662e244d8e8cc4c9fc8fa4b9e8f59fa2ee9927
Phionx <shanjha.usa@gmail.com>
2016-10-27 00:18:47 -0400

    tabs

--- randfile.c
+++ randfile.c
@@ -8,7 +8,7 @@
 #include <time.h> 
 
 int randInt() {
-    int fd = open("/dev/random", O_RDONLY);
+	int fd = open("/dev/random", O_RDONLY);
 	int ret;
 	read(fd, &ret, 4);
 	close(fd);
@@ -17,43 +17,42 @@ int randInt() {
 
 
 int main() {
-    srand(time(NULL));
-    umask(0);
+	srand(time(NULL));
+	umask(0);
 
 
 	//GENERATING NUMS
-    printf("Generating random numbers:\n");
-    int arr[10];
+	printf("Generating random numbers:\n");
+	int arr[10];
 	int i;
-    for(i = 0; i < 10; i++) {
-        arr[i] = randInt();
+	for(i = 0; i < 10; i++) {
+		arr[i] = randInt();
 		printf("\t\trandom %d: %d\n", i, arr[i]);
-    }
+	}
 
 	
 	//WRITING NUMS
-    printf("\nWriting numbers to file...\n");
+	printf("\nWriting numbers to file...\n");
 	int fileDest = open("randfile.txt", O_CREAT | O_RDWR, 0666);
 	int success = write(fileDest, &arr, 10 * 4);
 	if (success==-1) printf("%s\n", strerror(errno));
 	close(fileDest);
 
-	
 
 	//READING NUMS
-    printf("\nReading numbers from file...\n");
-    int fd = open("randfile.txt", O_RDONLY);
-    int readArr[10];
-    read(fd, readArr, 10 * 4);
+	printf("\nReading numbers from file...\n");
+	int fd = open("randfile.txt", O_RDONLY);
+	int readArr[10];
+	read(fd, readArr, 10 * 4);
 
 	//VERIFYING NUMS
 	printf("\nVerification that written values were the same:\n");
-    for(i = 0; i < 10; i++) {
-        printf("\t\trandom %d: %d", i, readArr[i]);
+	for(i = 0; i < 10; i++) {
+		printf("\t\trandom %d: %d", i, readArr[i]);
 		if(readArr[i] == arr[i]) printf("\tSAME\n");
 		else printf("\tDIFFERENT\n");
 	}
 
 
-    return 0;
+	return 0;
 }
