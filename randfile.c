450c1af6fce9a6735f2d71ccd94ea96ba0c03db7
Phionx <shanjha.usa@gmail.com>
2016-10-26 23:55:02 -0400

    DONE

--- randfile.c
+++ randfile.c
@@ -5,32 +5,53 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <time.h> 
 
-
-void populateArray(int *array) {
-    int fd = open("/dev/random", O_RDWR);
-    read(fd, array, 40);
-    write(fd, array, 40);
+int randInt() {
+    int fd = open("/dev/random", O_RDONLY);
+	int ret;
+	read(fd, &ret, 4);
+	return ret;
 }
+
 
 int main() {
     srand(time(NULL));
     umask(0);
+
+
+	//GENERATING NUMS
     printf("Generating random numbers:\n");
     int arr[10];
-    populateArray(arr);
-    int i;
+	int i;
     for(i = 0; i < 10; i++) {
-        printf("\t\trandom %d: %d\n", i, arr[i]);
+        arr[i] = randInt();
+		printf("\t\trandom %d: %d\n", i, arr[i]);
     }
+
+	
+	//WRITING NUMS
     printf("\nWriting numbers to file...\n");
-    int fd = open("randfile", O_CREAT | O_RDWR, 0644);
-    write(fd, arr, 10 * 4);
+	int fileDest = open("randNums", O_CREAT | O_RDWR, 0666);
+	int success = write(fileDest, &arr, 10 * 4);
+	if (success==-1) printf("%s\n", strerror(errno));
+	close(fileDest);
+
+	
+
+	//READING NUMS
     printf("\nReading numbers from file...\n");
+    int fd = open("randNums", O_RDONLY);
     int readArr[10];
     read(fd, readArr, 10 * 4);
-    printf("\nVerification that written values were the same:\n");
+
+	printf("\nVerification that written values were the same:\n");
     for(i = 0; i < 10; i++) {
-        printf("\t\trandom %d: %d\n", i, arr[i]);
-    }
+        printf("\t\trandom %d: %d", i, readArr[i]);
+		if(readArr[i] == arr[i]) printf("\tSAME\n");
+		else printf("\tDIFFERENT\n");
+	}
+
+
+    return 0;
 }
